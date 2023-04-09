0a63ef35d9a06fe6753b80ce1fd0e701f0477030
takashiono2 <takashiono710@gmail.com>
2023-04-10 00:53:53 +0900

    seciton5_46済
    readme_44_45_46

--- section5/my-app/src/UserList.tsx
+++ section5/my-app/src/UserList.tsx
@@ -1,13 +1,37 @@
 import "./UserList.css";
+import {useState} from "react"
 
 const UserList = () => {
+  const defaultUsers =[
+    { id: "1", name:"Mike", age:29 },
+    { id: "2", name:"Ken", age:30 },
+    { id: "3", name:"Mika", age:35 },
+    { id: "4", name:"Jhon", age:27 },
+  ];
+
+  const [users, setUsers] = useState(defaultUsers);
+
+  const addUser = (addUser: { id: string ,name: string, age: number }) => {
+    setUsers( (prevUsers) => [...prevUsers, addUser]);
+  };
+
   return (
-    <ul className="user-list-body">
-      <li>Example-1</li>
-      <li>Example-2</li>
-      <li>Example-3</li>
-    </ul>
+    <div>
+      <ul className="user-list-body">
+        { users.map((v)=>{
+          return (
+            <li key={v.id}>
+              {v.name} {v.age}
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={()=> addUser({ id: Math.random().toString() ,name: "John",age: 20 })}>
+        Add User
+      </button>
+    </div>
   );
+
 };
 
 export default UserList;
