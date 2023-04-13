f806f5bf63d69e437a1c379226754e5b2670770d
takashiono2 <takashiono710@gmail.com>
2023-04-14 00:16:08 +0900

    section7

---
+++ section7/my-app/src/App.tsx
@@ -0,0 +1,26 @@
+import styles from "./App.module.css";
+import { useState } from "react";
+import UserList from "./UserList";
+import UserForm from "./UserForm";
+
+function App() {
+  const defaultUsers = [
+    { id: "1", name: "John", age: 12 },
+    { id: "2", name: "Ken", age: 32 },
+    { id: "3", name: "Mika", age: 42 },
+    { id: "4", name: "Mac", age: 50 },
+  ];
+  const [users, setUsers] = useState(defaultUsers);
+  const addUser = (add: { id: string; name: string; age: number }) => {
+    setUsers((prevUsers) => [...prevUsers, add]);
+  };
+
+  return (
+    <div className={styles.body}>
+      <UserForm addUser={addUser}></UserForm>
+      <UserList users={users}></UserList>
+    </div>
+  );
+}
+
+export default App;
---
+++ section7/my-app/src/UserForm.tsx
@@ -0,0 +1,60 @@
+import styles from "./UserForm.module.css";
+import { useState } from "react";
+
+type Props = {
+  addUser: (user: { id: string; name: string; age: number }) => void;
+};
+
+const UserForm = (props: Props) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const [isValidName, setIsValidName] = useState(true);
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length >= 10) {
+      setIsValidName(false);
+    } else {
+      setIsValidName(true);
+    }
+    setName(e.target.value);
+  };
+
+  const onChangeAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const age = Number(e.target.value);
+    setAge(age);
+  };
+
+  const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    const user = { id: Math.random().toString(), name, age };
+    props.addUser(user);
+    setName("");
+    setAge(0);
+    console.log("onSubmit");
+  };
+
+  console.log("Render");
+
+  return (
+    <div>
+      <div className={styles.body}>
+        <form className={styles["content-form"]}>
+          <label>Name:</label>
+          <input
+            className={`${!isValidName && styles.invalid}`}
+            value={name}
+            onChange={(e) => onChangeName(e)}
+          ></input>
+          <label>Age:</label>
+          <input value={age} onChange={(e) => onChangeAge(e)}></input>
+          <button disabled={!isValidName} onClick={(e) => onSubmit(e)}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UserForm;
